401a61225ad4d3236154d0e85b0532825d8c52bb
hokieknight <dale.mrotek@gmail.com>
2023-12-13 15:36:42 -0500

    add card dealing functionality

--- game.py
+++ game.py
@@ -1,2 +1,32 @@
 """Game module"""
-CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+CARDS = [["A",11], ["2",2], ["3",3], ["4",4], ["5",5], ["6",6], ["7",7],
+         ["8",8], ["9",9], ["10",10], ["J",10], ["Q",10], ["K",10]]
+
+
+def deal_card():
+    """deal one random card"""
+    return CARDS[random.randint(0, 12)]
+
+def deal_hand():
+    """deal new hand"""
+    new_hand = []
+    new_hand.append(deal_card())
+    new_hand.append(deal_card())
+    return new_hand
+
+
+def hand_total(hand):
+    """Calculate hand total"""
+    total = 0
+    aces = 0
+    for card in hand:
+        total += card[1]
+        if card[0] == "A":
+            aces += 1
+    while total > 21 and aces > 0:
+        aces -= 1
+        total -= 10
+
+    return total
--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 """Blackjack"""
 import art
+import game
+
+
+def print_hand(name, hand, hide_first):
+    """display hand"""
+
+    display = name + ": "
+    is_first = True
+    for card in hand:
+        if is_first and hide_first:
+            display += "[*]"
+            is_first = False
+        else:
+            display += f"[{card[0]}]"
+    if not hide_first:
+        display += f" = {game.hand_total(hand)}"
+    print(display)
+
 
 def main():
     """main method"""
     print(art.LOGO)
+
+    player_hand = game.deal_hand()
+    dealer_hand = game.deal_hand()
+
+    print_hand("Dealer", dealer_hand, True)
+    print_hand("Player", player_hand, False)
+
+    #if input("(H)it or (S)tand? ") == "S":
+    print_hand("Dealer", dealer_hand, False)
+
 
 main()
