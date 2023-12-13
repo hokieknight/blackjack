c57740c2555fe13d8580e326e11d67e03c9ba45a
hokieknight <dale.mrotek@gmail.com>
2023-12-13 15:59:45 -0500

    simple game implementation

--- main.py
+++ main.py
@@ -29,8 +29,40 @@ def main():
     print_hand("Dealer", dealer_hand, True)
     print_hand("Player", player_hand, False)
 
-    #if input("(H)it or (S)tand? ") == "S":
+    player_done = False
+    player_lose = False
+    while not player_done:
+        action = input("(H)it or (S)tand? ")
+        if action == "H":
+            player_hand.append(game.deal_card())
+            print_hand("Player", player_hand, False)
+        elif action == "S":
+            player_done = True
+        if (game.hand_total(player_hand) > 21):
+            print("BUST.  You Lose!")
+            player_done = True
+            player_lose = True
+    
     print_hand("Dealer", dealer_hand, False)
+    dealer_done = False
+    dealer_lose = False
+    while not player_lose and not dealer_done:
+        if (game.hand_total(dealer_hand) < 17):
+            dealer_hand.append(game.deal_card())
+            print_hand("Dealer", dealer_hand, False)
+        else:
+            dealer_done = True
+        if (game.hand_total(dealer_hand) > 21):
+            print("Dealer BUST.  You Win!")
+            dealer_done = True
+            dealer_lose = True
 
+    if not player_lose and not dealer_lose:
+        if (game.hand_total(dealer_hand) > game.hand_total(player_hand)):
+            print("Dealer Wins!!")
+        elif (game.hand_total(dealer_hand) < game.hand_total(player_hand)):
+            print("Player Wins!!")
+        else:
+            print("Draw!!")
 
 main()
